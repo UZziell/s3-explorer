58799cda282bb3b978598e25b1707e27dec07425
uzziel <aliafifmm@gmail.com>
2025-02-03 12:50:50 +0330

    chore force running natively on OS

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import flet as ft
 
 if __name__ == "__main__":
     app = S3FileExplorerApp()
-    ft.app(target=app.main, view=ft.AppView.WEB_BROWSER)
+    # ft.app(target=app.main, view=ft.AppView.WEB_BROWSER)
+    ft.app(target=app.main)
